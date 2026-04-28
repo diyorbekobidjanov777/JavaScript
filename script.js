9ecbb5ee1c2cb552050a575116ae21160eca5451
diyorbekobidjanov777 <diyorbekobidjanov777>
2026-04-28 14:26:12 +0500

    update

---
+++ script.js
@@ -0,0 +1,75 @@
+// 1
+// let son = Number(prompt("Son kiriting:"));
+
+// if (son % 2 === 0) {
+//     console.log("Juft son");
+// } else {
+//     console.log("Toq son");
+// }
+
+
+// 2
+// let a = Number(prompt("1-son"));
+// let b = Number(prompt("2-son"));
+
+// if (a > b) {
+//     console.log("Katta son:", a);
+// } else {
+//     console.log("Katta son:", b);
+// }
+
+
+// 3
+// let tugilganYil = Number(prompt("Tug'ilgan yilingizni kiriting"));
+// let hozirgiYil = 2026;
+
+// let age = hozirgiYil - tugilganYil;
+
+// console.log("Yoshingiz:", age);
+
+
+// 4
+// let a = Number(prompt("1-son"));
+// let b = Number(prompt("2-son"));
+// let c = Number(prompt("3-son"));
+
+// let max = a;
+
+// if (b > max) {
+//     max = b;
+// }
+
+// if (c > max) {
+//     max = c;
+// }
+
+// console.log("Eng katta son:", max);
+
+
+// 5
+// let a = Number(prompt("1-sonni kiriting"));
+// let b = Number(prompt("2-sonni kiriting"));
+
+// console.log("Qo'shish:", a + b);
+// console.log("Ayirish:", a - b);
+// console.log("Ko'paytirish:", a * b);
+// console.log("Bo'lish:", a / b);
+
+
+// 6
+// let n = Number(prompt("n ni kiriting"));
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     sum = sum + i;
+// }
+
+// console.log("Yig'indi:", sum);
+
+
+// 7
+let n = Number(prompt("Son kiriting"));
+
+for (let i = 1; i <= 10; i++) {
+    console.log(n + " x " + i + " = " + (n * i));
+}
